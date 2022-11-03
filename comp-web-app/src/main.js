24ebd87cb0fcd552b4a7167c6b2521481611147b
Sascha Dietrich <sascha_dietrich@outlook.com>
2022-11-04 00:40:42 +0100

    feat: include validation formelement

--- comp-web-app/src/main.js
+++ comp-web-app/src/main.js
@@ -1,15 +1,17 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-import App from "./App.vue";
-import router from "./router";
+import App from "@/App.vue";
+import router from "@/router";
+import VeeValidatePlugin from "@/includes/validation.js";
 
-import "./assets/base.css";
-import "./assets/main.css";
+import "@/assets/base.css";
+import "@/assets/main.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VeeValidatePlugin);
 
 app.mount("#app");
